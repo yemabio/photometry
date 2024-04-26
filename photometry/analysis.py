7d1d1ddac5eebdcfdd2fa3f342853f56c94fc2ba
Andrew Ligeralde <ligeralde@berkeley.edu>
2024-04-26 14:37:30 -0700

    add median subtraction

--- photometry/analysis.py
+++ photometry/analysis.py
@@ -6,6 +6,7 @@ class DataFile:
     def __init__(self, 
                  filename, 
                  minutes_before_ttl_pulse=0,
+                 subtraction_mode='mean',
                  datatype=None,
                  frequency=0.0166,
                  z_norm=True,
@@ -14,6 +15,7 @@ class DataFile:
         self.filename = filename
         self.datatype=datatype
         self.minutes_before_ttl_pulse = minutes_before_ttl_pulse
+        self.subtraction_mode = subtraction_mode
         self.frequency=frequency
         self.z_norm = z_norm
         self.pre_injection_interval = self.convert_min_to_timesteps()
@@ -30,7 +32,7 @@ class DataFile:
     #     return returner(self.frequency)
     
     def extract_df_f(self):
-        extracted_df_f = get_df_f_from_doric(self.filename,z_norm=self.z_norm,subtract_pre_inj=self.pre_injection_interval)
+        extracted_df_f = get_df_f_from_doric(self.filename,z_norm=self.z_norm,subtract_pre_inj=self.pre_injection_interval, subtraction_mode=self.subtraction_mode)
     # for file_id, file_obj in file_dict.items():
     #  = analysis.get_df_f_from_doric(file_obj.filename,z_norm=True, subtract_pre_inj=file_obj.pre_injection_interval)
         if len(extracted_df_f) == 2:
@@ -66,20 +68,23 @@ def convert_sig_to_df_f(signal, baseline, z_norm=False):
 def get_pulse_end(ttl):
     return np.where(ttl == 1)[0][-1]
 
-def subtract_pre_ttl(time_series, ttl, pre_pulse_interval):
+def subtract_pre_ttl(time_series, ttl, pre_pulse_interval,subtraction_mode):
     pulse_end = np.where(ttl == 1)[0][-1]
     # print(pulse_end)
-    pre_ttl_mean = time_series[pulse_end+1-pre_pulse_interval:pulse_end+1].mean()
+    if subtraction_mode == "mean":
+        pre_ttl_subtraction = time_series[pulse_end+1-pre_pulse_interval:pulse_end+1].mean()
+    elif subtraction_mode == "median":
+        pre_ttl_subtraction = np.median(time_series[pulse_end+1-pre_pulse_interval:pulse_end+1])
     # print(pre_ttl_mean)
     # print(len(time_series-pre_ttl_mean))
     # return (time_series - pre_ttl_mean)[pulse_end+1:]
-    return(time_series - pre_ttl_mean)[pulse_end+1-pre_pulse_interval:]
+    return(time_series - pre_ttl_subtraction)[pulse_end+1-pre_pulse_interval:]
 
 
 def z_score(time_series):
     return (time_series-time_series.mean())/time_series.std()
 
-def get_df_f_from_doric(filename, z_norm=False, subtract_pre_inj=0):
+def get_df_f_from_doric(filename, z_norm=False, subtract_pre_inj=0, subtraction_mode="mean"):
     data = utils.get_signal_and_baseline(filename, get_ttl=subtract_pre_inj)
     signal = data[0]
     baseline = data[1]
@@ -97,7 +102,7 @@ def get_df_f_from_doric(filename, z_norm=False, subtract_pre_inj=0):
         # baseline = data[1]
     
     if subtract_pre_inj:
-        return (subtract_pre_ttl(df_f, data[2], pre_pulse_interval=subtract_pre_inj),get_pulse_end(data[2]))
+        return (subtract_pre_ttl(df_f, data[2], pre_pulse_interval=subtract_pre_inj,subtraction_mode=subtraction_mode),get_pulse_end(data[2]))
 
     else:
         return df_f
